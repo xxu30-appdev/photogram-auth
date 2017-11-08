82d51924d6263f22c5c21215557cf77550fa3eba
Murugan <murugu2012@gmail.com>
2017-11-08 13:27:33 +0530

    Generate photo image dynamically

--- spec/factories/photo.rb
+++ spec/factories/photo.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :photo do
-    image "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Lake_Bondhus_Norway_2862.jpg/1280px-Lake_Bondhus_Norway_2862.jpg"
+    image { Faker::Avatar.image }
     caption "Lake Bondhus"
   end
 end
