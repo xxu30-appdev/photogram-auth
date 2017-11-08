8db4ede1ed1c5b86505dc34ca9ff43ebc0c82e14
Murugan <murugu2012@gmail.com>
2017-11-08 15:29:48 +0530

    Break a test to multiple test

--- spec/features/4_mylikes_spec.rb
+++ spec/features/4_mylikes_spec.rb
@@ -11,35 +11,58 @@ feature "My_likes:" do
     expect(page.status_code).to be(200) # 200 is "OK"
   end
 
-  context "my likes" do
-    scenario "shows photos a user has liked", points: 1 do
-      user_1, user_2, user_3 = create_list(:user_with_photos, 3)
+  scenario "shows photos caption a user has liked", points: 1 do
+    user_1, user_2, user_3 = create_list(:user_with_photos, 3)
 
-      like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => user_1.photos.first.id)
-      like_2 = FactoryBot.create(:like, :user_id => user_3.id, :photo_id => user_3.photos.first.id)
-      login_as(user_2, :scope => :user)
-      visit "/my_likes"
+    like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => user_1.photos.first.id)
+    like_2 = FactoryBot.create(:like, :user_id => user_3.id, :photo_id => user_3.photos.first.id)
+    login_as(user_2, :scope => :user)
+    visit "/my_likes"
 
-      user_2.likes.each do |like|
-        expect(page).to have_content(like.photo.caption)
-        expect(page).to have_css("img[src*='#{like.photo.image}']")
-      end
+    user_2.likes.each do |like|
+      expect(page).to have_content(like.photo.caption)
     end
+  end
 
-    scenario "does not show photos a user hasn't liked", points: 1 do
-      user_1, user_2, user_3 = create_list(:user_with_photos, 3)
+  scenario "shows photos a user has liked", points: 1 do
+    user_1, user_2, user_3 = create_list(:user_with_photos, 3)
 
-      like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => user_1.photos.first.id)
-      like_2 = FactoryBot.create(:like, :user_id => user_3.id, :photo_id => user_3.photos.first.id)
+    like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => user_1.photos.first.id)
+    like_2 = FactoryBot.create(:like, :user_id => user_3.id, :photo_id => user_3.photos.first.id)
+    login_as(user_2, :scope => :user)
+    visit "/my_likes"
 
-      login_as(user_2, :scope => :user)
-      visit "/my_likes"
-
-      user_1.likes.each do |like|
-        expect(page).not_to have_content(like.photo.caption)
-        expect(page).not_to have_css("img[src*='#{like.photo.image}']")
-      end 
+    user_2.likes.each do |like|
+      expect(page).to have_css("img[src*='#{like.photo.image}']")
     end
+  end
+
+  scenario "does not show photos caption a user hasn't liked", points: 1 do
+    user_1, user_2, user_3 = create_list(:user_with_photos, 3)
+
+    like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => user_1.photos.first.id)
+    like_2 = FactoryBot.create(:like, :user_id => user_3.id, :photo_id => user_3.photos.first.id)
+
+    login_as(user_2, :scope => :user)
+    visit "/my_likes"
+
+    user_1.likes.each do |like|
+      expect(page).not_to have_content(like.photo.caption)
+    end 
+  end
+
+  scenario "does not show photos a user hasn't liked", points: 1 do
+    user_1, user_2, user_3 = create_list(:user_with_photos, 3)
+
+    like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => user_1.photos.first.id)
+    like_2 = FactoryBot.create(:like, :user_id => user_3.id, :photo_id => user_3.photos.first.id)
+
+    login_as(user_2, :scope => :user)
+    visit "/my_likes"
+
+    user_1.likes.each do |like|
+      expect(page).not_to have_css("img[src*='#{like.photo.image}']")
+    end 
   end
 
   scenario "header has link to /my_likes", points: 1 do
