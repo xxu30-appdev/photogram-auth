6e26edd8c68c5f6c8a874654b26fa42909d82fdc
Murugan <murugu2012@gmail.com>
2017-11-08 13:34:08 +0530

    Remove before block

--- spec/features/4_mylikes_spec.rb
+++ spec/features/4_mylikes_spec.rb
@@ -12,27 +12,37 @@ feature "My_likes:" do
   end
 
   context "my likes" do
-    before(:each) do
-      @user_1 = create(:user)
-      @user_2 = create(:user)
-      @photo_1 = FactoryBot.create(:photo, :user_id => @user_1.id)
-      @photo_2 = FactoryBot.create(:photo, :user_id => @user_1.id)
-      @photo_3 = FactoryBot.create(:photo, :user_id => @user_2.id)
-      @like_1 = FactoryBot.create(:like, :user_id => @user_2.id, :photo_id => @photo_1.id)
-      @like_2 = FactoryBot.create(:like, :user_id => @user_2.id, :photo_id => @photo_3.id)
-      login_as(@user_2, :scope => :user)
-      visit "/my_likes"
-    end
-
     scenario "shows photos a user has liked", points: 1 do
-      @user_2.likes.each do |like|
+      user_1 = create(:user)
+      user_2 = create(:user)
+      user_3 = create(:user)
+      photo_1 = FactoryBot.create(:photo, :user_id => user_1.id)
+      photo_2 = FactoryBot.create(:photo, :user_id => user_2.id)
+      photo_3 = FactoryBot.create(:photo, :user_id => user_3.id)
+      like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => photo_1.id)
+      like_2 = FactoryBot.create(:like, :user_id => user_3.id, :photo_id => photo_3.id)
+      login_as(user_2, :scope => :user)
+      visit "/my_likes"
+
+      user_2.likes.each do |like|
         expect(page).to have_content(like.photo.caption)
         expect(page).to have_css("img[src*='#{like.photo.image}']")
       end
     end
 
     scenario "does not show photos a user hasn't liked", points: 1 do
-      @user_1.likes.each do |like|
+      user_1 = create(:user)
+      user_2 = create(:user)
+      user_3 = create(:user)
+      photo_1 = FactoryBot.create(:photo, :user_id => user_1.id)
+      photo_2 = FactoryBot.create(:photo, :user_id => user_2.id)
+      photo_3 = FactoryBot.create(:photo, :user_id => user_3.id)
+      like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => photo_1.id)
+      like_2 = FactoryBot.create(:like, :user_id => user_3.id, :photo_id => photo_3.id)
+      login_as(user_2, :scope => :user)
+      visit "/my_likes"
+
+      user_1.likes.each do |like|
         expect(page).not_to have_content(like.photo.caption)
         expect(page).not_to have_css("img[src*='#{like.photo.image}']")
       end 
