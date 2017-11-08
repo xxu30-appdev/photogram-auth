51907b377b25b0a783b0f3f5a7f3144f72f9c4f1
Murugan <murugu2012@gmail.com>
2017-11-08 15:39:51 +0530

    Break a test to multiple test

--- spec/features/5_photos_spec.rb
+++ spec/features/5_photos_spec.rb
@@ -1,147 +1,166 @@
 require "rails_helper"
 
-feature "Photos:" do
+feature "Photos Index Page:" do
 
-  context "index page" do
+  scenario "only show edit link for signed-in user's photos", points: 1 do
+    owner = create(:user)
+    non_owner = create(:user)
 
-    scenario "only show edit link for signed-in user's photos", points: 1 do
-      owner = create(:user)
-      non_owner = create(:user)
+    login_as(owner, :scope => :user)
+    visit "/photos"
 
-      login_as(owner, :scope => :user)
-      visit "/photos"
-
-      owner.photos.each do |photo|
-        expect(page).to have_link(nil, href: "/photos/#{photo.id}/edit")
-      end
-
-      non_owner.photos.each do |photo|
-        expect(page).not_to have_link(nil, href: "/photos/#{photo.id}/edit")
-      end
+    owner.photos.each do |photo|
+      expect(page).to have_link(nil, href: "/photos/#{photo.id}/edit")
     end
 
-    scenario "only show delete link for signed-in user's photos", points: 1 do
-      owner = create(:user)
-      non_owner = create(:user)
-
-      login_as(owner, :scope => :user)
-      visit "/photos"
-
-      owner.photos.each do |photo|
-        expect(page).to have_link(nil, href: "/delete_photo/#{photo.id}")
-      end
-
-      non_owner.photos.each do |photo|
-        expect(page).not_to have_link(nil, href: "/delete_photo/#{photo.id}")
-      end
-    end
-
-    scenario "in /photos, ADD PHOTO link should present (and 'Photos' h1 tag isn't)", points: 1 do
-      user = create(:user)
-      photo = create(:photo, :user_id => user.id)
-      login_as(user, :scope => :user)
-
-      visit "/photos"
-
-      expect(page).to have_link("Add Photo", href: "/photos/new")
-      expect(page).not_to have_selector("h1", text: "Photos")
-    end
-
-    scenario "/photos displays per-photo username", points: 1 do
-      user = create(:user)
-      login_as(user, :scope => :user)
-
-      visit "/photos"
-      photos = Photo.all
-
-      photos.each do |photo|
-        expect(page).to have_content("#{photo.user.username}")
-      end
-    end
-
-    scenario "/photos displays photo", points: 1 do
-      user = create(:user)
-      login_as(user, :scope => :user)
-
-      visit "/photos"
-      photos = Photo.all
-
-      photos.each do |photo|
-        expect(page).to have_css("img[src*='#{photo.image}']")
-      end
-    end
-
-    scenario "/photos displays per-photo time elapsed", points: 1, hint: h("time_in_words") do
-      user = create(:user)
-      login_as(user, :scope => :user)
-
-      visit "/photos"
-      photos = Photo.all
-
-      photos.each do |photo|
-        expect(page).to have_content(ApplicationController.helpers.time_ago_in_words(photo.created_at))
-      end
-    end
-
-    scenario "header have Sign Out link", points: 1 do
-      user = create(:user)
-      login_as(user, :scope => :user)
-
-      visit "/photos"
-
-      within('nav') do
-        expect(page).to have_link("Sign Out", href: "/users/sign_out")
-      end
-    end
-
-    scenario "/photos lists comments with authors", points: 1 do
-      user_1, user_2, user_3 = create_list(:user_with_photos, 3)
-
-      comment_1 = FactoryBot.create(:comment, :user_id => user_1.id, :body => "comment_1", :photo_id => user_1.photos.first.id)
-      comment_2 = FactoryBot.create(:comment, :user_id => user_2.id, :body => "comment_two", :photo_id => user_2.photos.first.id)
-      login_as(user_3, :scope => :user)
-
-      visit "/photos"
-
-      photos = Photo.all
-      photos.each do |photo|
-        photo.comments.each do |comment|
-          expect(page).to have_content(comment.body)
-          expect(page).to have_content(comment.user.username)
-        end
-      end
-    end
-
-    scenario "/photos shows LIKE/UNLIKE button", points: 1 do
-      user_1 = create(:user_with_photos)
-      user_2 = create(:user_with_photos)
-
-      like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => user_1.photos.first.id)
-      like_2 = FactoryBot.create(:like, :user_id => user_2.id, :photo_id => user_2.photos.first.id)
-      login_as(user_1, :scope => :user)
-
-      visit "/photos"
-
-      expect(page).to have_css("button", text: 'Like')
-      
-      expect(page).to have_link("Unlike", href: "/delete_like/#{like_1.id}")
-    end
-
-    scenario "/photos includes form field with placeholder 'Add a comment...'", points: 1 do
-      user = create(:user)
-      login_as(user, :scope => :user)
-      photo = FactoryBot.create(:photo, :user_id => user.id)
-
-      visit "/photos"
-
-      expect(page).to have_selector("input[placeholder='Add a comment...']")
+    non_owner.photos.each do |photo|
+      expect(page).not_to have_link(nil, href: "/photos/#{photo.id}/edit")
     end
   end
-end
 
-feature "Photos:" do
+  scenario "only show delete link for signed-in user's photos", points: 1 do
+    owner = create(:user)
+    non_owner = create(:user)
 
-  scenario "quick-add a comment works and leads back to /photos", points: 2 do
+    login_as(owner, :scope => :user)
+    visit "/photos"
+
+    owner.photos.each do |photo|
+      expect(page).to have_link(nil, href: "/delete_photo/#{photo.id}")
+    end
+
+    non_owner.photos.each do |photo|
+      expect(page).not_to have_link(nil, href: "/delete_photo/#{photo.id}")
+    end
+  end
+
+  scenario "in /photos, ADD PHOTO link should present", points: 1 do
+    user = create(:user)
+    photo = create(:photo, :user_id => user.id)
+    login_as(user, :scope => :user)
+
+    visit "/photos"
+
+    expect(page).to have_link("Add Photo", href: "/photos/new")
+  end
+
+  scenario "in /photos, 'Photos' h1 tag should not present", points: 1 do
+    user = create(:user)
+    photo = create(:photo, :user_id => user.id)
+    login_as(user, :scope => :user)
+
+    visit "/photos"
+
+    expect(page).not_to have_selector("h1", text: "Photos")
+  end
+
+  scenario "/photos displays per-photo username", points: 1 do
+    user = create(:user)
+    login_as(user, :scope => :user)
+
+    visit "/photos"
+    photos = Photo.all
+
+    photos.each do |photo|
+      expect(page).to have_content("#{photo.user.username}")
+    end
+  end
+
+  scenario "/photos displays photo", points: 1 do
+    user = create(:user)
+    login_as(user, :scope => :user)
+
+    visit "/photos"
+    photos = Photo.all
+
+    photos.each do |photo|
+      expect(page).to have_css("img[src*='#{photo.image}']")
+    end
+  end
+
+  scenario "/photos displays per-photo time elapsed", points: 1, hint: h("time_in_words") do
+    user = create(:user)
+    login_as(user, :scope => :user)
+
+    visit "/photos"
+    photos = Photo.all
+
+    photos.each do |photo|
+      expect(page).to have_content(ApplicationController.helpers.time_ago_in_words(photo.created_at))
+    end
+  end
+
+  scenario "header have Sign Out link", points: 1 do
+    user = create(:user)
+    login_as(user, :scope => :user)
+
+    visit "/photos"
+
+    within('nav') do
+      expect(page).to have_link("Sign Out", href: "/users/sign_out")
+    end
+  end
+
+  scenario "/photos lists comments with authors", points: 1 do
+    user_1, user_2, user_3 = create_list(:user_with_photos, 3)
+
+    comment_1 = FactoryBot.create(:comment, :user_id => user_1.id, :body => "comment_1", :photo_id => user_1.photos.first.id)
+    comment_2 = FactoryBot.create(:comment, :user_id => user_2.id, :body => "comment_two", :photo_id => user_2.photos.first.id)
+    login_as(user_3, :scope => :user)
+
+    visit "/photos"
+
+    photos = Photo.all
+    photos.each do |photo|
+      photo.comments.each do |comment|
+        expect(page).to have_content(comment.user.username)
+      end
+    end
+  end
+
+  scenario "/photos lists comments in each photo", points: 1 do
+    user_1, user_2, user_3 = create_list(:user_with_photos, 3)
+
+    comment_1 = FactoryBot.create(:comment, :user_id => user_1.id, :body => "comment_1", :photo_id => user_1.photos.first.id)
+    comment_2 = FactoryBot.create(:comment, :user_id => user_2.id, :body => "comment_two", :photo_id => user_2.photos.first.id)
+    login_as(user_3, :scope => :user)
+
+    visit "/photos"
+
+    photos = Photo.all
+    photos.each do |photo|
+      photo.comments.each do |comment|
+        expect(page).to have_content(comment.body)
+      end
+    end
+  end
+
+  scenario "/photos shows LIKE/UNLIKE button", points: 1 do
+    user_1 = create(:user_with_photos)
+    user_2 = create(:user_with_photos)
+
+    like_1 = FactoryBot.create(:like, :user_id => user_1.id, :photo_id => user_1.photos.first.id)
+    like_2 = FactoryBot.create(:like, :user_id => user_2.id, :photo_id => user_2.photos.first.id)
+    login_as(user_1, :scope => :user)
+
+    visit "/photos"
+
+    expect(page).to have_css("button", text: 'Like')
+    
+    expect(page).to have_link("Unlike", href: "/delete_like/#{like_1.id}")
+  end
+
+  scenario "/photos includes form field with placeholder 'Add a comment...'", points: 1 do
+    user = create(:user)
+    login_as(user, :scope => :user)
+    photo = FactoryBot.create(:photo, :user_id => user.id)
+
+    visit "/photos"
+
+    expect(page).to have_selector("input[placeholder='Add a comment...']")
+  end
+
+  scenario "quick-add a comment should add comment", points: 2 do
     user_1 = create(:user_with_photos)
     user_2 = create(:user)
     login_as(user_2, :scope => :user)
@@ -153,6 +172,19 @@ feature "Photos:" do
     find("button", text: 'Add Comment').click
 
     expect(page).to have_content(new_comment)
+  end
+
+  scenario "after adding a comment page should leads back to /photos", points: 2 do
+    user_1 = create(:user_with_photos)
+    user_2 = create(:user)
+    login_as(user_2, :scope => :user)
+
+    visit "/photos"
+    
+    new_comment = "Just added a comment at #{Time.now.to_f}"
+    fill_in("Add a comment...", with: new_comment)
+    find("button", text: 'Add Comment').click
+
     expect(page).to have_current_path("/photos")
   end
 
